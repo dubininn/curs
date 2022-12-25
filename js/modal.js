ad621873dbd0d3bbd8392ff8e902b3d009c0a148
we <andrey.dubininn@gmail.com>
2022-12-25 23:58:59 +0300

    внёс правки

--- js/modal.js
+++ js/modal.js
@@ -9,3 +9,13 @@ enter.addEventListener('click', () => {
 close.addEventListener('click', () => {
     enterForm.classList.remove ('modal--visible');
 })
+
+const enterM = document.querySelector('.header__top-enter-320');
+
+enterM.addEventListener('click', () => {
+    enterForm.classList.add ('modal--visible');
+})
+
+close.addEventListener('click', () => {
+    enterForm.classList.remove ('modal--visible');
+})
